43c2196eb44255f03aede089782e483f4f44081f
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2024-06-22 18:13:15 +0300

    Update dgjhry.js

--- plugins/dgjhry.js
+++ plugins/dgjhry.js
@@ -1,13 +1,12 @@
-export async function all(m, conn) {
-   
+export async function all(m) {
   if (!m.isGroup)
-  return
+      return
   let chats = global.db.data.chats[m.chat]
   if (!chats.expired)
-  return !0
+      return !0
   if (+new Date() > chats.expired) {
-  
-  await m.reply(`Всем пока! бот выходит из группы, срок аренды бота истек`)
-  await this.groupLeave(m.chat)
-  chats.expired = null
-  }}
+      await this.reply(m.chat, `🦆 Прощайте, бот выходит из группы, срок аренды истек`)
+      await this.groupLeave(m.chat)
+      chats.expired = null
+  }
+}
