8f97e96174d143ea8c1abb44b99075ae8bc46897
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2024-07-03 19:40:36 +0300

    Update game-slot.js

--- plugins/game-slot.js
+++ plugins/game-slot.js
@@ -1,87 +1,37 @@
-/* CREDITOS A https://github.com/FG98F */
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    
+  let who
+  if (m.isGroup) who = args[1] ? args[1] : m.chat
+  else who = args[1]
 
-
-
-const handler = async (m, {args, usedPrefix, command}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
-  const tradutor = _translate.plugins.game_slot
-
-  const fa = `
-${tradutor.texto1} 
-
-${tradutor.texto2} 
-*${usedPrefix + command} 100*`.trim();
-  if (!args[0]) throw fa;
-  if (isNaN(args[0])) throw fa;
-  const apuesta = parseInt(args[0]);
-  const users = global.db.data.users[m.sender];
-  const time = users.lastslot + 10000;
-  if (new Date - users.lastslot < 10000) throw `${tradutor.texto3[0]} ${msToTime(time - new Date())} ${tradutor.texto3[1]}`;
-  if (apuesta < 100) throw tradutor.texto4;
-  if (users.exp < apuesta) {
-    throw tradutor.texto5;
-  }
-  const emojis = ['🐋', '🐉', '🕊️'];
-  let a = Math.floor(Math.random() * emojis.length);
-  let b = Math.floor(Math.random() * emojis.length);
-  let c = Math.floor(Math.random() * emojis.length);
-  const x = [];
-  const y = [];
-  const z = [];
-  for (let i = 0; i < 3; i++) {
-    x[i] = emojis[a];
-    a++;
-    if (a == emojis.length) a = 0;
-  }
-  for (let i = 0; i < 3; i++) {
-    y[i] = emojis[b];
-    b++;
-    if (b == emojis.length) b = 0;
-  }
-  for (let i = 0; i < 3; i++) {
-    z[i] = emojis[c];
-    c++;
-    if (c == emojis.length) c = 0;
-  }
-  let end;
-  if (a == b && b == c) {
-    end = `${tradutor.texto6} +${apuesta + apuesta} 𝚇𝙿*`;
-    users.exp += apuesta;
-  } else if (a == b || a == c || b == c) {
-    end = `${tradutor.texto7}`;
-    users.exp += 10;
-  } else {
-    end = `${tradutor.texto8} -${apuesta} 𝚇𝙿*`;
-    users.exp -= apuesta;
-  }
-  users.lastslot = new Date * 1;
-  return await m.reply(
-      `
-🎰 | *SLOTS* 
-────────
-${x[0]} : ${y[0]} : ${z[0]}
-${x[1]} : ${y[1]} : ${z[1]}
-${x[2]} : ${y[2]} : ${z[2]}
-────────
-🎰 | ${end}`);
-};
-handler.help = ['slot <apuesta>'];
-handler.tags = ['game'];
-handler.command = ['slot'];
-export default handler;
-
-function msToTime(duration) {
-  const milliseconds = parseInt((duration % 1000) / 100);
-  let seconds = Math.floor((duration / 1000) % 60);
-  let minutes = Math.floor((duration / (1000 * 60)) % 60);
-  let hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
-
-  hours = (hours < 10) ? '0' + hours : hours;
-  minutes = (minutes < 10) ? '0' + minutes : minutes;
-  seconds = (seconds < 10) ? '0' + seconds : seconds;
-
-  return minutes + ' m ' + seconds + ' s ';
+  if (new Date() * 1 < global.db.data.chats[who].expired) global.db.data.chats[who].expired = false
+  else global.db.data.chats[who].expired = false
+  
+  let caption = `**`
+  conn.sendButton(m.chat, caption, wm, null, [['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ ☘️', `/menu`]], m)
+  
 }
+handler.help = ['delexpired']
+handler.tags = ['owner']
+handler.command = /^(сбросАренды|delbottemporal|delbotemporal)$/i
+handler.owner = true
+handler.register = true
+handler.group = true
+
+export default handler
+
+function msToDate(ms) {
+  let temp = ms
+  let days = Math.floor(ms / (24 * 60 * 60 * 1000));
+  let daysms = ms % (24 * 60 * 60 * 1000);
+  let hours = Math.floor((daysms) / (60 * 60 * 1000));
+  let hoursms = ms % (60 * 60 * 1000);
+  let minutes = Math.floor((hoursms) / (60 * 1000));
+  let minutesms = ms % (60 * 1000);
+  let sec = Math.floor((minutesms) / (1000));
+  return days + " *Día(s)* ☀️\n" + hours + " *Hora(s)* ⏳\n" + minutes + " *Minuto(s)* ⏰\n" + sec + " *Segundo(s)* 🕐\n";
+  //return days + " Hari " + hours + " Jam " + minutes + " Menit";
+  // +minutes+":"+sec;
+}
+
 
