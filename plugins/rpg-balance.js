0756909a9747dabda81da14322c5136553e88ffd
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2024-07-03 17:52:25 +0300

    ыпр

--- plugins/rpg-balance.js
+++ plugins/rpg-balance.js
@@ -37,7 +37,7 @@ const handler = async (m, {usedPrefix}) => {
 
 `);
 };
-handler.customPrefix = /зр|@/i;
+handler.customPrefix = /з|@/i;
 handler.tags = ['anonymous'];
 handler.command = ['аказать'];
 handler.private = true;
