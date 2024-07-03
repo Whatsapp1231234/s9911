07e53d06dad619131d7e7040f16d453848bb32b4
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2024-07-03 18:16:08 +0300

    Update rpg-balance2.js

--- plugins/rpg-balance2.js
+++ plugins/rpg-balance2.js
@@ -1,45 +1,43 @@
-import fs from 'fs';
 
-const handler = async (m, { args, usedPrefix }) => {
-  const datas = global;
-  const idioma = datas.db.data.users[m.sender].language;
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`));
-  const tradutor = _translate.plugins.rpg_balance;
+
+const handler = async (m, {usedPrefix}) => {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const tradutor = _translate.plugins.rpg_balance
 
   let who;
-  if (m.isGroup) {
-    who = args[1] ? args[1] : m.chat;
-  } else {
-    who = args[1] ? args[1] : m.sender;
-  }
-
-  let name = "Пользователь";
-  if (who) {
-    name = conn.getName(who) || "Пользователь";
-  }
-
-  let caption = `
+  if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender;
+  else who = m.sender;
+  const name = conn.getName(who);
+  m.reply(`
 Здравствуйте. Имеется бот:
  _*Хулиган*_бот-админ
 °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
 *Присутствуют маты*.
 °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
- _*Хулиган*_ вызывает всю группу через @, удаляет за ссылки, сносит арабов и индусов, закрывает и открывает группу по команде.
+ _*Хулиган*_ вызывает всю группу через @ , удаляет за ссылки, сносит арабов и индусов,  закрывает и открывает группу по команде. 
 ╔════════
 ╠══ *ЦЕНА БОТА*
-╠  Хулиган - 500₽ месяц
+╠  Хулиган - 500₽  месяц 
 ╚════════
-Ответственность за работу бота я не несу. Так как бот это программа и бывает виснет, на сервере перегруз, перебой. Также ватсап блокирует номера и бот не исключение. В случае блокировки бота, деньги не возвращаю, другого не предоставляю, так как это не моя вина.
+Ответственность за работу бота я не несу. Так как бот это программа и бывает виснет, на сервере перегруз, перебой. Так же ватсап блокирует номера и бот не исключение. В случае блокировки бота, деньги не возвращаю, другого не предоставляю, так как это не моя вина.
 Если вас все устраивает, скидывайте ссылку на группу.
 ╔════════
 ╠══ *Оплата*
 ╠   https://www.sberbank.com/sms/pbpn?requisiteNumber=79384030507
+╠ 
+╠ 
+╠ 
 ╠══ *Свяжитесь с создателем*
 ╠ #создатель
+╠ 
 ╚════════
-`;
-};
 
+
+
+`);
+};
 handler.customPrefix = /а|@/i;
 handler.tags = ['anonymous'];
 handler.command = ['ренда'];
